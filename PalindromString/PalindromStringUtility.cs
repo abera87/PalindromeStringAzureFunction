56896418341c7f0e96a9ffee78fb773fb81c1d0b
fstvmuser <fstvmuser@vm1>
2021-04-23 12:51:26 +0530

    Add project files.

---
+++ PalindromString/PalindromStringUtility.cs
@@ -0,0 +1,28 @@
+﻿namespace PalindromString
+{
+    public class PalindromStringUtility
+    {
+        public static bool CheckPalindromString(string word)
+        {
+            bool result = false;
+            if (word.Length == 0) return result;
+            else if (word.Length == 1) result = true;
+            else
+            {
+                result = Palindrom(word.ToUpper(), 0, word.Length - 1);
+            }
+            return result;
+        }
+        static bool Palindrom(string word, int start, int end)
+        {
+            if (start == end)
+                return true;
+            else if (word[start] != word[end])
+                return false;
+            else if (start < end + 1)
+                return Palindrom(word, start + 1, end - 1);
+
+            return true;
+        }
+    }
+}
